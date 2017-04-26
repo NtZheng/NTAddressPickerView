2d0abac4b02d39feb6c133884eceab64553b2c3c
NtZheng <2745674085@qq.com>
2017-04-26 14:21:14 +0800

    初始化了项目，封装了一个二级的地址选择器

---
+++ NTAddressPickerView/NTAddressPickerView/NTAddressPickerView.h
@@ -0,0 +1,13 @@
+//
+//  NTAddressPickerView.h
+//  NTAddressPickerView
+//
+//  Created by nineteen on 4/26/17.
+//  Copyright © 2017 nineteen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NTAddressPickerView : UIView
+
+@end
---
+++ NTAddressPickerView/NTAddressPickerView/NTAddressPickerView.m
@@ -0,0 +1,21 @@
+//
+//  NTAddressPickerView.m
+//  NTAddressPickerView
+//
+//  Created by nineteen on 4/26/17.
+//  Copyright © 2017 nineteen. All rights reserved.
+//
+
+#import "NTAddressPickerView.h"
+
+@implementation NTAddressPickerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
