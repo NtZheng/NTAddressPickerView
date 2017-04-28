0629285d56e5a1d7ced54a02387599211b7bf412
NtZheng <2745674085@qq.com>
2017-04-28 15:00:41 +0800

    1、新建了markdown说明文件

--- NTAddressPickerView/NTAddressPickerView/NTAddressPickerView.h
+++ NTAddressPickerView/NTAddressPickerView/NTAddressPickerView.h
@@ -13,6 +13,12 @@ typedef void(^ConfirmBlock)(NSString *province, NSString *town);
 
 @interface NTAddressPickerView : UIView
 
+/*
+ * @param animated : 是否进行动画
+ * @param cancelBlock : 点击取消按钮所进行的操作（block）
+ * @param confirmBlock : 点击确认按钮所进行的操作（block）
+ */
+
 + (void)showAddressPickerViewWithAnimated :(BOOL)animated cancelAction: (CancelBlock)cancelBlock confirmBlock: (ConfirmBlock)confirmBlock;
 
 @end
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # NTAddressPickerView
+#####这是一个二级的地址选择器
+
+* 使用方法
+
+```
+/*
+ * @param animated : 是否进行动画
+ * @param cancelBlock : 点击取消按钮所进行的操作（block）
+ * @param confirmBlock : 点击确认按钮所进行的操作（block）
+ */
+
++ (void)showAddressPickerViewWithAnimated :(BOOL)animated cancelAction: (CancelBlock)cancelBlock confirmBlock: (ConfirmBlock)confirmBlock;
+
+```
+* 效果展示
+
+![示意图](示意图.gif)
